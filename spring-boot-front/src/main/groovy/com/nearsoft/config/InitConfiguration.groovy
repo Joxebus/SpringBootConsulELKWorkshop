28dcfe2a43d7041c4d98ae7bf6277b5ab31bbba6
Omar Bautista <obautista@skytouchtechnology.com>
2019-07-11 11:50:52 -0700

    Add configuration to monitor with Consul

--- spring-boot-front/src/main/groovy/com/nearsoft/config/InitConfiguration.groovy
+++ spring-boot-front/src/main/groovy/com/nearsoft/config/InitConfiguration.groovy
@@ -2,6 +2,7 @@ package com.nearsoft.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -13,6 +14,7 @@ class InitConfiguration {
     String rootUri;
 
     @Bean
+    @LoadBalanced
     RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.rootUri(rootUri).build();
     }
--- spring-boot-service/src/main/groovy/com/nearsoft/BackApplication.groovy
+++ spring-boot-service/src/main/groovy/com/nearsoft/BackApplication.groovy
@@ -3,12 +3,14 @@ package com.nearsoft
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 /**
  * This is the main class of the project
  */
 
 @SpringBootApplication( scanBasePackages = ['com.nearsoft'], exclude = [SolrAutoConfiguration])
+@EnableDiscoveryClient
 class BackApplication {
 
     static void main(String[] args) {
